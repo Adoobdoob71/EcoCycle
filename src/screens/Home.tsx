0a49b7e88b9def920549a769653e5187a2560301
Adoobdoob71 <eladelad71@gmail.com>
2021-07-01 17:29:22 +0300

    👌 IMPROVE: upgraded UI

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -9,12 +9,20 @@ import {
   Dimensions,
 } from 'react-native';
 import {useTheme} from 'react-native-paper';
-import {Header, Card, IconButton, Top} from '../components';
+import {
+  Header,
+  Card,
+  IconButton,
+  Top,
+  PeerProgress,
+  Row,
+  Column,
+} from '../components';
 import {TouchableOpacity} from 'react-native-gesture-handler';
 import {AnimatedCircularProgress} from 'react-native-circular-progress';
 import {useNavigation} from '@react-navigation/native';
-import {DrawerNavigationProp} from '@react-navigation/drawer';
 import {BarChart} from 'react-native-chart-kit';
+import {convertOpacityToHex} from '../utils/usefulFunctions';
 
 const Home: React.FC = () => {
   const {colors} = useTheme();
@@ -24,52 +32,94 @@ const Home: React.FC = () => {
   const openDrawer = () => navigation.openDrawer();
   return (
     <SafeAreaView style={styles.mainView}>
-      <Header
-        left={<IconButton icon="menu" onPress={openDrawer} />}
-        right={
-          <TouchableOpacity>
-            <Image
-              source={{
-                uri: 'https://avatars.githubusercontent.com/u/46420655?v=4',
-              }}
-              style={styles.headerProfilePicture}
+      <ScrollView
+        style={{flex: 1}}
+        stickyHeaderIndices={[0]}
+        showsVerticalScrollIndicator={false}
+        bounces={false}
+        overScrollMode="never">
+        <Header
+          left={
+            <IconButton
+              icon="menu"
+              size={21}
+              onPress={openDrawer}
+              innerStyle={{backgroundColor: colors.background}}
+              borderless
             />
-          </TouchableOpacity>
-        }
-        title={`Welcome back, Elad`}
-        subtitle="Planning on recycling more?"
-      />
-      <ScrollView style={{flex: 1}}>
+          }
+          right={
+            <View style={{flexDirection: 'row', alignItems: 'center'}}>
+              <IconButton icon="barcode-scan" onPress={() => {}} borderless />
+              <TouchableOpacity style={{marginStart: 8}}>
+                <Image
+                  source={{
+                    uri: 'https://avatars.githubusercontent.com/u/46420655?v=4',
+                  }}
+                  style={styles.headerProfilePicture}
+                />
+              </TouchableOpacity>
+            </View>
+          }
+          backgroundStyle={{
+            backgroundColor: colors.surface,
+            borderRadius: 8,
+            margin: 6,
+            elevation: 4,
+          }}
+          title={`Welcome back, Elad`}
+          subtitle="Planning on recycling more?"
+        />
         <View style={styles.contentView}>
           <Card
             onPress={() => {}}
             innerStyle={{
-              justifyContent: 'center',
-              alignItems: 'center',
               padding: 12,
-              marginVertical: 12,
             }}>
-            <AnimatedCircularProgress
-              rotation={0}
-              size={100}
-              width={2}
-              fill={(10 / 25) * 100}
-              tintColor={colors.primary}
-              backgroundColor={colors.placeholder}
-              style={{marginVertical: 12}}>
-              {fill => <Text style={styles.recycledBottlesAmount}>10/25</Text>}
-            </AnimatedCircularProgress>
-            <Text style={styles.recycledBottlesGreeting}>
-              Almost halfway there!
-            </Text>
+            <Row style={{alignItems: 'center', justifyContent: 'space-around'}}>
+              <Column style={{alignItems: 'center'}}>
+                <AnimatedCircularProgress
+                  rotation={0}
+                  size={100}
+                  width={2}
+                  fill={(10 / 25) * 100}
+                  tintColor={colors.primary}
+                  backgroundColor={colors.placeholder}
+                  style={{marginVertical: 12}}>
+                  {fill => (
+                    <Text style={styles.recycledBottlesAmount}>10/25</Text>
+                  )}
+                </AnimatedCircularProgress>
+                <Text style={styles.recycledBottlesGreeting}>
+                  Bottles recycled
+                </Text>
+              </Column>
+              <Column style={{alignItems: 'center'}}>
+                <AnimatedCircularProgress
+                  rotation={0}
+                  size={100}
+                  width={2}
+                  fill={53}
+                  tintColor={colors.primary}
+                  backgroundColor={colors.placeholder}
+                  style={{marginVertical: 12}}>
+                  {fill => (
+                    <Text style={styles.recycledBottlesAmount}>53%</Text>
+                  )}
+                </AnimatedCircularProgress>
+                <Text style={styles.recycledBottlesGreeting}>
+                  53% items recycled
+                </Text>
+              </Column>
+            </Row>
           </Card>
           <Card
             onPress={() => {}}
-            innerStyle={{
+            outerStyle={{
               marginVertical: 12,
             }}>
-            <Top textStyle={{margin: 4}}>
-              In the last 4 days you've recycled:
+            <Top textStyle={{margin: 4, marginVertical: 2}}>
+              Recycling history
             </Top>
             <BarChart
               data={{
@@ -81,23 +131,50 @@ const Home: React.FC = () => {
                 ],
               }}
               // withInnerLines={false}
-              // showValuesOnTopOfBars
+              yLabelsOffset={32}
               yAxisLabel=""
               yAxisSuffix=""
+              fromZero
               width={Dimensions.get('window').width - 48}
               height={200}
               chartConfig={{
                 backgroundGradientFrom: colors.surface,
                 backgroundGradientFromOpacity: 0,
-                backgroundGradientTo: colors.surface,
+                backgroundGradientTo: colors.primary,
                 backgroundGradientToOpacity: 0,
                 decimalPlaces: 0,
-                color: opacity => `rgba(139, 255, 113, ${opacity})`,
+                color: opacity => colors.primary + convertOpacityToHex(opacity),
                 barPercentage: 0.5,
                 useShadowColorFromDataset: false, // optional
               }}
-              style={{marginVertical: 8, alignSelf: 'center'}}
+              style={{marginVertical: 8, alignSelf: 'center', borderRadius: 8}}
             />
+          </Card>
+          <Card onPress={() => {}} outerStyle={{marginBottom: 12}}>
+            <Top style={{margin: 4, marginVertical: 2}}>
+              Here's where you stand amongst your peers
+            </Top>
+            <View style={{margin: 8}}>
+              <PeerProgress
+                nickname="Elad Mekonen"
+                profile_picture="https://avatars.githubusercontent.com/u/46420655?v=4"
+                progressValue={0.64}
+                outerStyle={{marginBottom: 6}}
+                isUser
+              />
+              <PeerProgress
+                nickname="Devin Booker"
+                profile_picture="https://wallpaperaccess.com/full/5457303.png"
+                progressValue={0.4}
+                outerStyle={{marginBottom: 6}}
+              />
+              <PeerProgress
+                nickname="Chris Paul"
+                profile_picture="https://64.media.tumblr.com/644ea9b4a3baa8d8b6c362422686fb34/tumblr_pg90ks4V2s1xu7jxzo1_1280.png"
+                progressValue={0.8}
+                outerStyle={{marginBottom: 6}}
+              />
+            </View>
           </Card>
         </View>
       </ScrollView>
