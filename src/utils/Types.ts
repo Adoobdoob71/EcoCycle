530c5fcbee94c899986b73c6865f1d1f41616246
Adoobdoob71 <eladelad71@gmail.com>
2021-07-05 21:40:38 +0300

    👌 IMPROVE: new UserData interface

--- src/utils/Types.ts
+++ src/utils/Types.ts
@@ -1,3 +1,9 @@
 import { StyleProp, ViewStyle } from 'react-native';
 
 export type StyleProperty = StyleProp<ViewStyle>; 
+
+export interface UserData {
+	nickname?: string;
+	username?: string;
+	profile_picture?: string;
+}
