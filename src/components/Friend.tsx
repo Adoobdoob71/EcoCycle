8d439842fd7772bd3f0eaed4cbc028467ba048f9
Adoobdoob71 <eladelad71@gmail.com>
2021-07-09 20:20:40 +0300

    👌 IMPROVE: look of Friends screen

--- src/components/Friend.tsx
+++ src/components/Friend.tsx
@@ -1,11 +1,72 @@
 import React from 'react';
+import {View, Text, StyleSheet, Image} from 'react-native';
+import {useTheme} from 'react-native-paper';
+import {StyleProperty} from '../utils/Types';
+import {RippleButton, Column, Row} from './';
 
 interface FriendProps {
-	
+  nickname?: string;
+  username?: string;
+  profile_picture?: string;
+  description?: string;
+  uid?: string;
+  onPress?: () => void;
+  outerStyle?: StyleProperty;
 }
 
-const Friend: React.FC<FriendProps> = (props) => {
-	return null;
-}
+const Friend: React.FC<FriendProps> = props => {
+  const {colors} = useTheme();
+  const styles = classes(colors);
 
-export default Friend
+  return (
+    <RippleButton
+      onPress={props.onPress ? props.onPress : () => {}}
+      borderRadius={0}
+      innerStyle={styles.background}
+      outerStyle={props.outerStyle}>
+      <Image
+        source={{uri: props.profile_picture}}
+        style={styles.friendProfilePicture}
+      />
+      <Column style={{flex: 1}}>
+        <Row style={{alignItems: 'center', marginBottom: 1}}>
+          <Text style={styles.friendNickname}>{props.nickname}</Text>
+          <Text style={styles.friendUsername}>@{props.username}</Text>
+        </Row>
+        <Text style={styles.friendDescripton}>{props.description}</Text>
+      </Column>
+    </RippleButton>
+  );
+};
+
+function classes(colors: any) {
+  return StyleSheet.create({
+    background: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      paddingHorizontal: 12,
+      paddingVertical: 6,
+    },
+    friendProfilePicture: {
+      width: 40,
+      height: 40,
+      borderRadius: 20,
+      marginEnd: 12,
+    },
+    friendNickname: {
+      fontSize: 16,
+      color: colors.text,
+      fontWeight: 'bold',
+      marginEnd: 4,
+    },
+    friendUsername: {
+      fontSize: 14,
+      color: colors.placeholder,
+    },
+    friendDescripton: {
+      fontSize: 12,
+      color: colors.text,
+    },
+  });
+}
+export default Friend;
--- src/screens/Friends.tsx
+++ src/screens/Friends.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {SafeAreaView, View, Text, StyleSheet} from 'react-native';
 import {useTheme} from 'react-native-paper';
 import {ScrollView} from 'react-native-gesture-handler';
-import {Header, IconButton} from '../components';
+import {Header, IconButton, Friend} from '../components';
 import {useNavigation} from '@react-navigation/native';
 import {UserData} from '../utils/Types';
 
@@ -28,6 +28,21 @@ const Friends: React.FC = () => {
         <Header
           left={<IconButton icon="menu" onPress={openDrawer} borderless />}
           title="Friends"
+          backgroundStyle={{marginBottom: 8}}
+        />
+        <Friend
+          profile_picture="https://i.pinimg.com/originals/08/87/33/088733465f7c684a6cdd242eb463b5bf.jpg"
+          nickname="DoritoWizard"
+          username="DoritoWizard71"
+          description="I like programming"
+          outerStyle={{marginBottom: 8}}
+        />
+        <Friend
+          profile_picture="https://i.pinimg.com/originals/08/87/33/088733465f7c684a6cdd242eb463b5bf.jpg"
+          nickname="DoritoWizard"
+          username="DoritoWizard71"
+          description="I like programming"
+          outerStyle={{marginBottom: 8}}
         />
       </ScrollView>
     </SafeAreaView>
