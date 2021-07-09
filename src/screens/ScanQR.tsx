8735a3b87cd00dc3c1f6940f1685fceb693f3f65
Adoobdoob71 <eladelad71@gmail.com>
2021-07-09 20:23:11 +0300

    📦 NEW: ScanQR screen

---
+++ src/screens/ScanQR.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import {SafeAreaView, StyleSheet} from 'react-native';
+import {useTheme} from 'react-native-paper';
+import {Header, IconButton} from '../components';
+import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
+import RecieptScanner from '../fragments/RecieptScanner';
+import {useNavigation} from '@react-navigation/native';
+import NFCScanner from '../fragments/NFCScanner';
+
+const Tab = createMaterialTopTabNavigator();
+
+const ScanQR: React.FC = () => {
+  const {colors} = useTheme();
+  const styles = classes(colors);
+  const navigation = useNavigation();
+  const goBack = () => navigation.goBack();
+  return (
+    <>
+      <Header
+        left={<IconButton icon="arrow-left" onPress={goBack} borderless />}
+        title="Scan"
+      />
+      <Tab.Navigator
+        initialRouteName="Reciepts"
+        style={{backgroundColor: colors.surface}}>
+        <Tab.Screen component={RecieptScanner} name="Reciepts" />
+        <Tab.Screen component={NFCScanner} name="NFC" />
+      </Tab.Navigator>
+    </>
+  );
+};
+
+function classes(colors: any) {
+  return StyleSheet.create({
+    background: {
+      backgroundColor: colors.background,
+    },
+  });
+}
+
+export default ScanQR;
