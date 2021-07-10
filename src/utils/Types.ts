5d8f835b4b7d2c4ef58595cd4d37fab122f7ce95
Adoobdoob71 <eladelad71@gmail.com>
2021-07-10 23:47:22 +0300

    👌 IMPROVE: changed UserData type

--- src/utils/Types.ts
+++ src/utils/Types.ts
@@ -3,7 +3,10 @@ import { StyleProp, ViewStyle } from 'react-native';
 export type StyleProperty = StyleProp<ViewStyle>; 
 
 export interface UserData {
-	nickname?: string;
-	username?: string;
-	profile_picture?: string;
+	id: string;
+	name: string | null;
+	email: string;
+	photo: string | null;
+	familyName: string | null;
+	givenName: string | null;
 }
