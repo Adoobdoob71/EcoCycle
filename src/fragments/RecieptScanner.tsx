c7bc2ff0612a9e8ea45081b19f13f5c42b17d9c1
Adoobdoob71 <eladelad71@gmail.com>
2021-07-12 17:37:01 +0300

    👌 IMPROVE: can scan barcodes now

--- src/fragments/RecieptScanner.tsx
+++ src/fragments/RecieptScanner.tsx
@@ -1,12 +1,49 @@
 import React from 'react';
 import {SafeAreaView, StyleSheet, View, Image} from 'react-native';
 import {useTheme} from 'react-native-paper';
-import {RNCamera} from 'react-native-camera';
+import {BarCodeReadEvent, RNCamera} from 'react-native-camera';
 import {Dimensions} from 'react-native';
+import {RecyclingDataType} from '../utils/Types';
+import firebase from 'firebase/app';
+import {AuthContext} from '../utils/Auth';
+import {watermelonDatabase} from '../..';
+import {RecycleProgress} from '../utils/model/Models';
+import {useNavigation} from '@react-navigation/native';
 
 const RecieptScanner: React.FC = () => {
+  const [scanned, setScanned] = React.useState(false);
+
   const {colors} = useTheme();
   const styles = classes(colors);
+
+  const {userInfo} = React.useContext(AuthContext);
+  const navigation = useNavigation();
+
+  const readBarcode = async (event: BarCodeReadEvent) => {
+    if (scanned) return;
+    const barCodeData = JSON.parse(event.data);
+    await recordData(barCodeData);
+    navigation.goBack();
+  };
+
+  const recordData = async (barCodeData: RecyclingDataType) => {
+    try {
+      const recyclingData = watermelonDatabase.get('items_recycled');
+      await watermelonDatabase.action(async () => {
+        const data = await recyclingData.create((item: any) => {
+          item._raw.all_items = barCodeData.allItems;
+          item._raw.bottles = barCodeData.bottles;
+          item._raw.plastic_items = barCodeData.plasticItems;
+          item._raw.metallic_items = barCodeData.metallicItems;
+          item._raw.paper_items = barCodeData.paperItems;
+        });
+      });
+    } catch (error) {
+      console.error(error);
+    }
+    setScanned(true);
+  };
+
   return (
     <SafeAreaView style={{flex: 1}}>
       <RNCamera
@@ -17,7 +54,8 @@ const RecieptScanner: React.FC = () => {
           message: 'We need your permission to use your camera',
           buttonPositive: 'Ok',
           buttonNegative: 'Cancel',
-        }}>
+        }}
+        onBarCodeRead={readBarcode}>
         <View style={styles.focusSquare}></View>
       </RNCamera>
     </SafeAreaView>
